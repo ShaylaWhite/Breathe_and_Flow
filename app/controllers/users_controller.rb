22f23fc1bff0cfb396e16498589e2d86d4400dd7
ShaylaWhite <shayladwhite1@gmail.com>
2020-09-08 15:23:56 -0700

    sessions controller and routes and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,28 @@ class UsersController < ApplicationController
      end
 
      def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user_id
+            redirect_to @user
+        else
+           render :new 
+        end 
      end 
-
+   
+     def index
+      
+     end 
 
      def show
      end 
+
+     private
+
+     def user_params
+        params.require(:user).permit(:username, :email, :password)
+     end 
+
+     #gives access to the params
 
 end
