ca084ceac19d9fcb5137f041f4670e51e15eccda
ShaylaWhite <shayladwhite1@gmail.com>
2020-09-08 10:39:16 -0700

    user model & review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+    belongs_to :users
+    belongs_to :groups
+
+    #join table, has two belongs to
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :groups
+    has_many :reviews
+    has_many :reviewed_groups, through :reviews 
+    ##has_many :studios, through :groups
 end
