57d4a8c7da7c2e186323361279ff3a422b94eaa7
ShaylaWhite <shayladwhite1@gmail.com>
2020-09-08 11:55:16 -0700

    bcrypt gem and has_secured password

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :user
-  belongs_to :studio
   has_many :reviews
   has_many :users, through: :reviews
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :groups
     has_many :reviews
     has_many :reviewed_groups, through: :reviews, source: :group
+    has_secure_password #authenicate, validate password &/or password confirmation
     ##has_many :studios, through :groups
 end
