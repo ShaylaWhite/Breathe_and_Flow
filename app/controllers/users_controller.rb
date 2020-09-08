58d0701dcb0fc2ccdb27c581657138423aa06653
ShaylaWhite <shayladwhite1@gmail.com>
2020-09-08 16:01:40 -0700

    users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,10 +15,12 @@ class UsersController < ApplicationController
      end 
    
      def index
-      
+
      end 
 
      def show
+      @user = User.find_by_id(params[:id])
+      redirect_to '/' if !@user
      end 
 
      private
