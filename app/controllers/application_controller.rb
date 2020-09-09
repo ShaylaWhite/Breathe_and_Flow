6640d244d4f8e71bf13992048ecfafb1264c927e
ShaylaWhite <shayladwhite1@gmail.com>
2020-09-08 17:48:03 -0700

    nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
 
+    helper_method :logged_in?
+    helper_method :create_user
+    helper_method :require_login
+    #now the views can use the helper methods
+
     def current_user
         @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,3 @@
 class GroupsController < ApplicationController
+   before_action :require_login
 end
